25dc3ea410135c58abf8acdf6847022db0e44fde
Cuong Nguyen <cuong@taureau.ai>
2022-08-09 17:38:25 +0700

    update

--- tutorial/tutorial_django/views.py
+++ tutorial/tutorial_django/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-from rest_framework import viewsets, permissions
-from .models import Course
-from .serializers import CourseSerializer
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from .models import Course, Lesson, Tag
+from .serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -21,8 +24,21 @@ class CourseViewSet(viewsets.ModelViewSet):
     # ... (PUT) --> cap nhap
     # ... (DELETE) --> xoa khoa hoc
 
+class LessonViewSet(viewsets.ModelViewSet):
+    queryset = Lesson.objects.filter(active=True)
+    serializer_class = LessonSerializer
 
+    @action(methods=['post'], detail=True, url_path="hide-lesson")
+    # /lessons/{pk}/hide_lesson
+    def hide_lesson(self, request, pk):
+        try:
+            lesson = Lesson.objects.get(pk=pk)
+            lesson.active = False
+            lesson.save()
+        except Lesson.DoesNotExits:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
+        return Response(data=LessonSerializer(lesson, context={'request': request}).data, status=status.HTTP_200_OK)
 
 
 def index(request):
